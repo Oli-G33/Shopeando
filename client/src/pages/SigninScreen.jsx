3e3d024d2aa7cc2b539f86a5cfe7b4ad5b0e1b02
OliGarc333 <ngarcia333@gmail.com>
2022-08-01 21:55:54 +0200

    Added sign in features, login navbar state

--- client/src/pages/SigninScreen.jsx
+++ client/src/pages/SigninScreen.jsx
@@ -7,7 +7,7 @@ import { Helmet } from 'react-helmet-async';
 import React, { useContext, useEffect, useState } from 'react';
 import { Store } from '../Store';
 // import { toast } from 'react-toastify';
-import { getError } from '../utils';
+// import { getError } from '../utils';
 
 export default function SigninScreen() {
   const navigate = useNavigate();
